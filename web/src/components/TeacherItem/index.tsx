d87f1391c332a7408a60cde3e71116314ae8b5e5
Isaque Maestri <isaquemaestri01@gmail.com>
2020-08-08 19:42:50 -0300

    commit

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,34 +2,61 @@ import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css'
+import api from '../../services/api';
 
-export default function TeacherItem() {
+export interface Teacher {
+
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection(){
+        api.post('connections',{
+            user_id: teacher.id
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/60708221?s=460&u=cf765082a65e5a7eef95994e6efaade3f90b0b01&v=4" alt="perfil" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Isaque Maestri</strong>
-                    <span> Algoritmos</span>
+                    <strong>{teacher.name}</strong>
+                    <span> {teacher.subject}</span>
                 </div>
             </header>
             <p>
-                What is Lorem Ipsum?
-                <br />  <br />
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                {teacher.bio}
             </p>
             <footer>
                 <p>
                     Preço/Hora
-                <strong>R$ 80, 00</strong>
+                <strong>{teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a 
+                    onClick={createNewConnection}
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                    target="_blank"
+                >
                     <img src={whatsappIcon} alt="contato" />
                         Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
 }
+
+
+export default TeacherItem;
